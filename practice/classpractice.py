27935176278cb381f65a07f2503cf393fe8bb699
Richard Zhao <richardz@andrew.cmu.edu>
2015-02-19 10:10:52 -0500

    Added new directories

---
+++ practice/classpractice.py
@@ -0,0 +1,38 @@
+class MyClass:
+
+    i = 12345
+
+    def __init__(self):
+        self.data = []
+
+    def f(self):
+        return 'hello world'
+
+class Complex:
+
+    def __init__(self, realpart, imagpart):
+        self.r = realpart
+        self.i = imagpart
+
+class Dog:
+
+    kind = 'canine'
+
+    def __init__(self, name):
+        self.name = name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
