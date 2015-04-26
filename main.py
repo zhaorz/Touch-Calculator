7f99f02a6e7f6c2126c8969850ef7f955ec51f98
Richard Zhao <richardz@andrew.cmu.edu>
2015-04-26 00:56:04 -0400

    Started working on main.py

--- main.py
+++ main.py
@@ -3,10 +3,32 @@ main.py
 ~~~~~~~~~~~~~~~
 
 
+WolframAlpha AppID: RAJVVX-8GEGHXV5LU
+
 """
 
 
 
+import eventBasedAnimation
+
+import classifier
+
+
+
+
+
+class MainWindow(eventBasedAnimation.Animation):
+
+    def onInit(self): pass
+    def onMouse(self, event): pass
+    def onKey(self, event): pass
+    def onStep(self): pass
+    def onDraw(self, canvas): pass
+    def onMouseMove(self, event): pass
+    def onMouseDrag(self, event): pass
+    def onMouseRelease(self, event): pass
+    def onKeyRelease(self, event): pass
+    def onQuit(self): pass
 
 
 
@@ -25,6 +47,31 @@ main.py
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+width = 700
+height = 700
+timerDelay = 64
+MainWindow(width=width, height=height, timerDelay=timerDelay).run()
 
 
 
